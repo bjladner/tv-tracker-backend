783ca2224e6dc2e837a8336d8cff0a148a2a1ffe
Brian Ladner <git@theladners.com>
2025-09-10 00:51:01 -0500

    updated

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ const apiRoutes = require('./routes/apiRoutes');
 const app = express();
 
 // CONNECT TO MONGO DATABASE
-mongoose.connect('mongodb://127.0.0.1:27017/tvShows')
+// You can use environment variables for db configuration
+const dbUsername = process.env.DB_USERNAME || 'mongoose';
+const dbPassword = process.env.DB_PASSWORD || 'mongoose';
+const dbAddress = process.env.DB_ADDRESS || '127.0.0.1';
+const dbPort = process.env.DB_PORT || '27017';
+const dbName = process.env.DB_NAME || 'tvShows'; 
+const dbLocal = process.env.DB_LOCAL || true;
+if (dbLocal) {
+  const dbURL = `mongodb://${dbAddress}:${dbPort}/${dbName}`;
+} else {
+  const dbURL = `mongodb://${dbUsername}:${dbPassword}@${dbAddress}:${dbPort}/${dbName}`;
+}
+mongoose.connect(dbURL)
 .then(function () {
   console.log('DB Connection Open!');
 })
@@ -20,7 +32,8 @@ app.use(cors())
 app.use('/api', apiRoutes)
 
 // CREATE EXPRESS SERVER
-const port = process.env.PORT || 3010; // You can use environment variables for port configuration
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// You can use environment variables for api configuration
+const apiPort = process.env.API_PORT || 3000;
+app.listen(apiPort, () => {
+    console.log(`Server is running on port ${apiPort}`);
 });
