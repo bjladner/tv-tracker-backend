5ceab70a0c7abe4aed46bd8f912e2c304a5f7cef
Brian Ladner <git@theladners.com>
2025-09-10 11:38:31 -0500

    updated

--- index.js
+++ index.js
@@ -16,42 +16,22 @@ const dbAddress = process.env.DB_ADDRESS || '127.0.0.1';
 const dbPort = process.env.DB_PORT || '27017';
 const dbName = process.env.DB_NAME || 'tvShows'; 
 const dbLocal = process.env.DB_LOCAL || true;
-//
-console.log(process.env.DB_LOCAL);
-console.log(dbLocal);
-console.log(process.env.DB_USERNAME);
-console.log(dbUsername);
-console.log(process.env.DB_PASSWORD);
-console.log(dbPassword);
-console.log(process.env.DB_ADDRESS);
-console.log(dbAddress);
-console.log(process.env.DB_PORT);
-console.log(dbPort);
-console.log(process.env.DB_NAME);
-console.log(dbName);
-const dbURL = (dbLocal === true) ? `mongodb://${dbAddress}:${dbPort}/${dbName}` : `mongodb://${dbUsername}:${dbPassword}@${dbAddress}:${dbPort}/${dbName}`;
-var dbURL2 = '';
-if (dbLocal === true) {
-  dbURL2 = `mongodb://${dbAddress}:${dbPort}/${dbName}`;
-} else {
-  dbURL2 = `mongodb://${dbUsername}:${dbPassword}@${dbAddress}:${dbPort}/${dbName}`;
-}
-console.log(`dbURL = ${dbURL}`);
-console.log(`dbURL2 = ${dbURL2}`);
-//
+const dbURL = (dbLocal === true) ? 
+  `mongodb://${dbAddress}:${dbPort}/${dbName}` : 
+  `mongodb://${dbUsername}:${dbPassword}@${dbAddress}:${dbPort}/${dbName}`;
 mongoose.connect(dbURL)
 .then(function () {
   console.log('DB Connection Open!');
-  console.log(dbURL);
 })
 .catch(function (error) {
   console.log(error);
 })
 
 // CREATE EXPRESS VIEWS AND ROUTES
+const apiRoute = process.env.API_ROUTE || '/api';
 app.use(express.json());
 app.use(cors())
-app.use('/api', apiRoutes)
+app.use(apiRoute, apiRoutes)
 
 // CREATE EXPRESS SERVER
 // You can use environment variables for api configuration
